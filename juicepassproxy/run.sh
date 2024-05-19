17243f25b46cffb07ec0c2d1d4c3cddcd5873b4b
user <wozz@koh.ms>
2024-05-18 22:14:27 -0400

    export

--- juicepassproxy/run.sh
+++ juicepassproxy/run.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bashio
 
-MQTT_HOST=$(bashio::services mqtt "host")
-MQTT_USER=$(bashio::services mqtt "username")
-MQTT_PASS=$(bashio::services mqtt "password")
+export MQTT_HOST="$(bashio::services mqtt 'host')"
+export MQTT_USER="$(bashio::services mqtt 'username')"
+export MQTT_PASS="$(bashio::services mqtt 'password')"
 
-JUICEBOX_HOST=$(bashio::config 'juicebox_host')
-UPDATE_UDPC=true
+export JUICEBOX_HOST="$(bashio::config 'juicebox_host')"
+export UPDATE_UDPC=true
 
-JPP_HOST=$(bashio::config 'ha_host')
+export JPP_HOST="$(bashio::config 'ha_host')"
 
-DEVICE_NAME=$(bashio::config 'juicebox_device_name')
+export DEVICE_NAME="$(bashio::config 'juicebox_device_name')"
 
 /juicepassproxy/docker_entrypoint.sh
